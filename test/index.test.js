46d00572c0006d3d8d398259e32dd2f456c2f6ba
Trey Shugart <treshugart@gmail.com>
2017-09-11 23:40:06 +1000

    Get tests passing.

--- test/index.test.js
+++ test/index.test.js
@@ -1,22 +1,22 @@
 /** @jsx h */
 
-import { h } from "preact";
-import { withRenderer } from ".";
+import { h } from 'preact';
+import { withRenderer } from '../src';
 
 class MyElement extends withRenderer() {
   renderCallback({ name }) {
-    return h("div", null, "Hello, ", name, "!");
+    return h('div', null, 'Hello, ', name, '!');
   }
 }
-customElements.define("my-element", MyElement);
+customElements.define('my-element', MyElement);
 
-describe("@skatejs/renderer-preact", () => {
-  it("renders", () => {
+describe('@skatejs/renderer-preact', () => {
+  it('renders', () => {
     const el = new MyElement();
-    expect(el.innerHTML).toEqual("");
-    el.rendererCallback(el, el.renderCallback.bind(el, { name: "World" }));
-    expect(el.innerHTML).toEqual("<div>Hello, World!</div>");
-    el.rendererCallback(el, el.renderCallback.bind(el, { name: "Bob" }));
-    expect(el.innerHTML).toEqual("<div>Hello, Bob!</div>");
+    expect(el.innerHTML).toEqual('');
+    el.rendererCallback(el, el.renderCallback.bind(el, { name: 'World' }));
+    expect(el.innerHTML).toEqual('<div>Hello, World!</div>');
+    el.rendererCallback(el, el.renderCallback.bind(el, { name: 'Bob' }));
+    expect(el.innerHTML).toEqual('<div>Hello, Bob!</div>');
   });
 });
