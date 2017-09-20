b46bec6c168c5fc4bd551811f9958e7288c032a4
Trey Shugart <treshugart@gmail.com>
2017-09-20 23:01:33 +1000

    Fix transpile error.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { render } from 'preact';
 
-export default Base =>
-  class extends (Base || HTMLElement) {
+export default (Base = HTMLElement) =>
+  class extends Base {
     rendererCallback(renderRoot, renderCallback) {
       this._preactDom = render(
         renderCallback(),
