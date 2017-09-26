9ed276ee113a95eea5192041b0612472979a8d7f
Trey Shugart <treshugart@gmail.com>
2017-09-26 11:33:14 +1000

    Bring Preact renderer in line with React renderer.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import { render } from 'preact';
+/** @jsx h */
+
+import { h, render } from 'preact';
 
 export default (Base = HTMLElement) =>
   class extends Base {
+    get props() {
+      // We override props so that we can satisfy most use
+      // cases for children by using a slot.
+      return {
+        ...super.props,
+        ...{ children: <slot /> }
+      };
+    }
     rendererCallback(renderRoot, renderCallback) {
       this._preactDom = render(
         renderCallback(),
--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 /** @jsx h */
 
-import { h } from 'preact';
+import { Component, h } from 'preact';
 import withRenderer from '../src';
 
 class MyElement extends withRenderer() {
@@ -10,13 +10,42 @@ class MyElement extends withRenderer() {
 }
 customElements.define('my-element', MyElement);
 
-describe('@skatejs/renderer-preact', () => {
-  it('renders', () => {
-    const el = new MyElement();
-    expect(el.innerHTML).toEqual('');
-    el.rendererCallback(el, el.renderCallback.bind(el, { name: 'World' }));
-    expect(el.innerHTML).toEqual('<div>Hello, World!</div>');
-    el.rendererCallback(el, el.renderCallback.bind(el, { name: 'Bob' }));
-    expect(el.innerHTML).toEqual('<div>Hello, Bob!</div>');
-  });
+test('renders', () => {
+  function testContent(text) {
+    return `<div>Hello, ${text}!</div>`;
+  }
+
+  const el = new MyElement();
+  expect(el.innerHTML).toEqual('');
+  el.rendererCallback(el, el.renderCallback.bind(el, { name: 'World' }));
+  expect(el.innerHTML).toEqual(testContent('World'));
+  el.rendererCallback(el, el.renderCallback.bind(el, { name: 'Bob' }));
+  expect(el.innerHTML).toEqual(testContent('Bob'));
+});
+
+test('wrappers', () => {
+  class PreactComponent extends Component {
+    render() {
+      return <div>Hello, {this.props.children}!</div>;
+    }
+  }
+
+  class PreactComponentWrapper extends withRenderer() {
+    constructor() {
+      super();
+      this.attachShadow({ mode: 'open' });
+    }
+    renderCallback() {
+      return <PreactComponent {...this.props} />;
+    }
+  }
+
+  customElements.define('preact-component-wrapper', PreactComponentWrapper);
+
+  const el = new PreactComponentWrapper();
+  const { shadowRoot } = el;
+  el.rendererCallback(shadowRoot, el.renderCallback.bind(el));
+  expect(shadowRoot.innerHTML).toEqual(
+    '<div>Hello, <slot></slot>!</div>'
+  );
 });
